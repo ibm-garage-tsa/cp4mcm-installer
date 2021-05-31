955af06febf7a67945386636d1d339de91e5863d
Bright Zheng <bright.zheng@outlook.com>
2021-05-31 19:59:58 +0800

    fix and enhance ansible scripts

--- cp4m/ansible.sh
+++ cp4m/ansible.sh
@@ -14,50 +14,29 @@ execlog cloudctl login -a $YOUR_CP4MCM_ROUTE --skip-ssl-validation -u admin -p $
 #
 # Create Ansible AWX Resources
 #
+
+# Create a dedicated ansible project
 log "Creating Ansible project"
 execlog oc new-project ansible
 
 log "Updating Ansible project SCC"
 execlog oc adm policy add-scc-to-user privileged -z awx -n ansible
 
+# Create AWX Operator
 log "Creating AWX Operator Deployment"
-
-oc apply -f - << EOF
-kind: ClusterRoleBinding
-apiVersion: rbac.authorization.k8s.io/v1
-metadata:
-  name: awx-operator
-subjects:
-  - kind: ServiceAccount
-    name: awx-operator
-    namespace: ansible
-roleRef:
-  kind: ClusterRole
-  name: awx-operator
-  apiGroup: rbac.authorization.k8s.io
-
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: awx-operator
-  namespace: ansible
-EOF
-
-
-oc apply -f https://raw.githubusercontent.com/ansible/awx-operator/0.9.0/deploy/awx-operator.yaml
+curl -Lo $LOGDIR/awx-operator.yaml https://raw.githubusercontent.com/ansible/awx-operator/0.9.0/deploy/awx-operator.yaml
+sed "s/namespace: default/namespace: ansible/" $LOGDIR/awx-operator.yaml > $LOGDIR/awx-operator-updated.yaml
+oc apply -f $LOGDIR/awx-operator-updated.yaml
 
 log "Creating TLS secret for secure route"
-
 log "Extracting Let's Encrypt certificate from Openshift Ingress"
 ingress_pod=$(oc get secrets -n openshift-ingress | grep tls | grep -v router-metrics-certs-default | awk '{print $1}')
-oc get secret -n openshift-ingress -o 'go-template={{index .data "tls.crt"}}' ${ingress_pod} | base64 -d > cert.crt
-oc get secret -n openshift-ingress -o 'go-template={{index .data "tls.key"}}' ${ingress_pod} | base64 -d > cert.key
+oc get secret -n openshift-ingress -o 'go-template={{index .data "tls.crt"}}' ${ingress_pod} | base64 -d > $LOGDIR/cert.crt
+oc get secret -n openshift-ingress -o 'go-template={{index .data "tls.key"}}' ${ingress_pod} | base64 -d > $LOGDIR/cert.key
+oc -n ansible create secret generic tls-secret  --from-file=tls.crt=$LOGDIR/cert.crt  --from-file=tls.key=$LOGDIR/cert.key --save-config --dry-run=client -o yaml | oc apply -f -
 
-oc -n ansible create secret generic tls-secret  --from-file=tls.crt=./cert.crt  --from-file=tls.key=./cert.key --save-config --dry-run=client -o yaml | oc apply -f -
-
+# Create AWX CR
 log "Creating AWX Instance"
-
 oc apply -f - <<EOF
 apiVersion: awx.ansible.com/v1beta1
 kind: AWX
@@ -101,15 +80,23 @@ log "Sleeping while AWX is starting.. (600 seconds)"
 progress-bar 600
 
 #
+# Expose the service as route
+#
+execlog oc -n ansible expose service awx-service --name=awx
+
+#
 # Configure CP4MCM Menu
 #
 log "Configuring Menu integration"
-sed "s/ansible-tower-web-svc/awx/" cp4m/automation-navigation-updates.sh > awx.sh
-chmod +x awx.sh
-./awx.sh -a ansible
+sed "s/ansible-tower-web-svc/awx/" cp4m/automation-navigation-updates.sh > $LOGDIR/awx.sh
+chmod +x $LOGDIR/awx.sh
+$LOGDIR/awx.sh -a ansible
 
 #
-# List out what users in "operations" group that we've imported
+# Print out the login credentials
 #
-log "Listing out the AWX admin user credentials."
-execlog oc -n ansible get secret awx-admin-password -o jsonpath="{.data.password}" | base64 --decode
+log "Printing out the AWX credentials."
+log "You can access the AWX with:"
+log " - URL: `oc get -n ansible route awx -o jsonpath='{.spec.host}'`"
+log " - Admin User: admin"
+log " - Admin Password: `oc -n ansible get secret awx-admin-password -o jsonpath='{.data.password}' | base64 --decode`"
